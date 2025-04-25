39375e44366d2346217a9ce28f4cfadf3932a586
Riddhin2004 <144728625+Riddhin2004@users.noreply.github.com>
2025-04-25 14:21:30 +0530

    Create LandRent.jsx

---
+++ ContractFarming/LandRent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LandRent = () => {
+  return (
+    <div>
+        <div className="max-w-7xl mx-auto px-6 flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4"></div>
+      <h1>HELLOO</h1>
+      <p>Here is some textt</p>
+    </div>
+  )
+}
+
+export default LandRent
